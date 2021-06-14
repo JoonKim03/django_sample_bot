882ac5313e503cf409ebb1bc321b6136cd568f96
JoonKim03 <jkim9203@gmail.com>
2021-06-15 08:05:33 +0900

    first commit

---
+++ sample_bot/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for sample_bot project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_bot.settings')
+
+sys.path.append('/home/ubuntu')
+sys.path.append('/home/ubuntu/myvenv/lib/python3.8/site-packages')
+
+application = get_wsgi_application()
+
---
+++ sample_bot_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SampleBotAppConfig(AppConfig):
+    name = 'sample_bot_app'
