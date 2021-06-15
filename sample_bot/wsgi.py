8a1f7066b0574077a98866223b8befb76be4054b
JoonKim03 <jkim9203@gmail.com>
2021-06-15 13:12:02 +0900

    third commit

--- sample_bot/wsgi.py
+++ sample_bot/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_bot.settings')
 
-sys.path.append('/home/ubuntu/django')
+sys.path.append('/home/ubuntu/django/django_sample_bot')
 sys.path.append('/home/ubuntu/django/myvenv/lib/python3.8/site-packages')
 
 application = get_wsgi_application()
