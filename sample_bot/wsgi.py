de54736c45a9c3ea60a463edf7a204cbcfc11004
JoonKim03 <jkim9203@gmail.com>
2021-06-15 09:35:22 +0900

    'second_commit'

--- sample_bot/wsgi.py
+++ sample_bot/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_bot.settings')
 
 sys.path.append('/home/ubuntu')
-sys.path.append('/home/ubuntu/myvenv/lib/python3.8/site-packages')
+sys.path.append('/home/ubuntu/django/myvenv/lib/python3.8/site-packages')
 
 application = get_wsgi_application()
 
